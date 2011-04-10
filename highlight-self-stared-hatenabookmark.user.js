c05cdb5b6dbc7d4a827183d562b6646b08753db1
brainfs <darknesssharp@gmail.com>
2011-04-10 17:25:09 +0900

    support for compressed star container

--- highlight-self-stared-hatenabookmark.user.js
+++ highlight-self-stared-hatenabookmark.user.js
@@ -5,14 +5,8 @@
 // @description highlight self-stared bookmark comment on b.hatena.ne.jp page
 // @include http://b.hatena.ne.jp/entry?mode=more*
 // ==/UserScript==
-
-//
-// TODO: 14以上のスターがついてるとき、短縮されるので、その処理
-//
-
-
-(function()
-{
+//test http://b.hatena.ne.jp/entry?mode=more&url=http%3A%2F%2Fhatena.g.hatena.ne.jp%2Fhatenabookmark%2F20110405%2F1301981564
+(function(){
 
 var textColor = '#ffff00'
 var backColor = '#000000'
@@ -30,21 +24,14 @@ function userList()
     return ret
 }
 
-function homelink(domelem)
+function username(domelem)
 {
-    return 'http://b.hatena.ne.jp/' + domelem.getAttribute('data-user') + '/';
+    return domelem.getAttribute('data-user')
 }
 
-function containSelfStar(domelem)
+function homelink(domelem)
 {
-    var slst = domelem
-          .getElementsByTagName('span')[4]
-          .getElementsByTagName('a');
-    var ulink = homelink(domelem);
-    for (var i = 0; i < slst.length; i++)
-        if (ulink == slst[i].getAttribute('href'))
-            return true
-    return false
+    return 'http://b.hatena.ne.jp/' + username(domelem) + '/';
 }
 
 function highlightComment(domelem)
@@ -55,17 +42,77 @@ function highlightComment(domelem)
         )
 }
 
+function starContainer(domelem)
+{
+    var lst = domelem.getElementsByTagName('span')
+    for (var i = 0; i < lst.length; i++)
+        if (lst[i].hasAttribute('class') && lst[i].getAttribute('class') == 'hatena-star-star-container')
+            return lst[i]
+}
+
+function highlightIfSelfStarAsync(domelem)
+{
+    var path = domelem
+                .getElementsByTagName('a')[1]
+                .getAttribute('href')
+    var uri = 'http://b.hatena.ne.jp' + path
+    var name = username(domelem)
+
+    GM_xmlhttpRequest({
+        method: 'GET',
+        url: 'http://s.hatena.ne.jp/entry.json?uri=' + encodeURIComponent(uri),
+        onload: function(res) {
+            var lst = eval('(' + res.responseText + ')').entries[0].stars
+            for (var i = 0; i < lst.length; i++)
+                if (lst[i].name == name)
+                {
+                    highlightComment(domelem)
+                    break
+                }
+        }
+    })
+}
+
+function isCompressed(starContainer)
+{
+    // has over thirteen stars?
+    return starContainer.getElementsByTagName('span').length > 0
+}
+
+function highlightIfSelfStar(domelem)
+{
+    if (isCompressed(starContainer(domelem)))
+    {
+        highlightIfSelfStarAsync(domelem)
+        return
+    }
+    var slst = domelem
+          .getElementsByTagName('span')[4]
+          .getElementsByTagName('a')
+    var ulink = homelink(domelem)
+    for (var i = 0; i < slst.length; i++)
+        if (ulink == slst[i].getAttribute('href'))
+        {
+            highlightComment(domelem)
+            return
+        }
+}
+
 function main()
 {
     var lst = userList();
     for (var i = 0; i < lst.length; i++)
-        if (containSelfStar(lst[i]))
-        {
-            //alert(lst[i].getAttribute('data-user'));
-            highlightComment(lst[i])
-        }
+        highlightIfSelfStar(lst[i])
+}
+
+
+function search(lst, name)
+{
+    for (var i = 0; i < lst.length; i++)
+        if (username(lst[i]) == name)
+            return lst[i]
 }
 
 
 main()
-})();
+})()
